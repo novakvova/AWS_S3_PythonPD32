5fc60ce5560663a9a78e7f0268ae65e312d932fb
novakvova <novakvova@gmail.com>
2026-02-17 17:48:33 +0200

    Working begin push

--- main.py
+++ main.py
@@ -20,3 +20,11 @@ s3_client.put_bucket_versioning(
     Bucket='my-girl-images', 
     VersioningConfiguration={'Status': 'Enabled'}
 ) # Enable versioning on a bucket
+
+response = s3_client.list_object_versions(
+    Bucket='my-girl-images',
+    Prefix='info.txt'
+) # List object versions in a bucket
+
+for version in response.get('Versions'):
+    print(version)
